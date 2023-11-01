e7bb82d323eabd8572ee7250cdf1c655fb298b1a
onebyte2016 <onebyte2016@gmail.com>
2023-11-01 04:18:31 -0700

    print aphabet without e and q

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for char in range(ord('a'), ord('z') + 1):
-   char = chr(char)
-   if char != 'e' and char != 'q':
-      print("{}".format(char), end="")
+    char = chr(char)
+    if char != 'e' and char != 'q':
+     print("{}".format(char), end="")
