c38c65abde49c7da8c17060d6b7de7e9e4bfbe23
onebyte2016 <onebyte2016@gmail.com>
2023-11-01 09:07:58 -0700

    print comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(i + 1, 10):
-        print("{:d}" "{:d}".format(i, j), end=", " if j < 99 else "\n")
+for i in range(10):
+    for j in range(1, 10):
+        if i < j:
+          print("{:d}{:d}".format(i, j), end="\n " if i == 8 and j == 9 else ", ")
