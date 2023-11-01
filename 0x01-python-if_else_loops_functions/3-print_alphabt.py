285e02fadaeeaea66e0699af2bed505f3c364e91
onebyte2016 <onebyte2016@gmail.com>
2023-11-01 04:44:00 -0700

    print

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for char in range(ord('a'), ord('z') + 1):
+  if char != 'e' and char != 'q':
     char = chr(char)
-    if char != 'e' and char != 'q':
-       print("{}".format(char), end="")
+    print("{}".format(char), end="")
