ca52c937ae7c5413dd1cf692951f1f7bcf384125
onebyte2016 <onebyte2016@gmail.com>
2023-11-06 14:50:59 -0800

    palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -21,7 +21,7 @@ int aux_palind(listint_t **head, listint_t *end)
 	if (end == NULL)
 		return (1);
 	if (aux_palind(head, end->next) && (*head)->n == end->n)
-	}
+	{
 		*head = (*head)->next;
 		return (1);
 	}
