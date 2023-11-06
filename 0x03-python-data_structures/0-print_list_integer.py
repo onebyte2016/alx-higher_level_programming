bf473a693584ca2b956fb58b7333ac9f509613fc
onebyte2016 <onebyte2016@gmail.com>
2023-11-06 07:36:22 -0800

    print integer number

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,5 @@
+#!usr/bin/python3
+def print_list_integer(my_list=[]):
+    """print the list of integers"""
+    for i in my_list:
+        print("{}".format(i))
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,1 @@
+the readme for python data structure
