eb878f41383c1e0fbeaf3dc7fb121423efc97de9
onebyte2016 <onebyte2016@gmail.com>
2023-11-07 01:01:53 -0800

    print python list

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -9,7 +9,7 @@ void print_python_list_info(PyObject *p)
 PyObject *obj;
 size = Py_SIZE(p);
 alloc = ((PyListObject *)p)->allocated;
-printf("[*] Size of the Python = %d\n", size);
+printf("[*] Size of the Python List = %d\n", size);
 printf("[*] Allocated = %d\n", alloc);
 
 for (i = 0; i < size; i++)
