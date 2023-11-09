db32d5c73f927290263a56a340d3f2d2be72ee38
onebyte2016 <onebyte2016@gmail.com>
2023-11-08 18:15:46 -0800

    python print list

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -27,10 +27,12 @@ void print_python_list(PyObject *p)
 			print_python_bytes(list->ob_item[i]);
 	}
 }
+
 /**
  * print_python_bytes - Prints basic info about python byte objects
  * @p: A PyObject byte object
  */
+
 void print_python_bytes(PyObject *p)
 {
 	unsigned char i, size;
