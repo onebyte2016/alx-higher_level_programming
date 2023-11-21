ed16fc437db6f7c20139f8913ce79a569e245827
onebyte2016 <onebyte2016@gmail.com>
2023-11-20 19:51:48 -0800

    python c

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <python.h>
+#include <Python.h>
 #include <floatobject.h>
-
 /**
  * print_python_float - prints into about python float
  * @p: address of python struct
  */
-
 void print_python_float(PyObject *p)
 {
 	double d;
+
 	setbuf(stdout, NULL);
 	printf("[.] float object info\n");
 	if (strcmp(p->ob_type->tp_name, "float"))
@@ -19,7 +18,8 @@ void print_python_float(PyObject *p)
 		return;
 	}
 	d = ((PyFloatObject *)p)->ob_fval;
-	printf("  value: %s\n", PyOS_double_to_string(d, 'r', 0, Py_DTSF_ADD_DOT_0, NULL));
+	printf("  value: %s\n",
+			PyOS_double_to_string(d, 'r', 0, Py_DTSF_ADD_DOT_0, NULL));
 }
 /**
  * print_python_bytes - prints info about python bytes
@@ -47,15 +47,14 @@ void print_python_bytes(PyObject *p)
 		printf("%02hhx%s", str[i], i + 1 < len ? " " : "");
 	printf("\n");
 }
-
 /**
  * print_python_list - print info about list
  * @p: first parameter
  */
-
 void print_python_list(PyObject *p)
 {
 	int i;
+
 	setbuf(stdout, NULL);
 	printf("[*]  Python list info\n");
 	if (strcmp(p->ob_type->tp_name, "list"))
@@ -63,11 +62,13 @@ void print_python_list(PyObject *p)
 		printf("  [ERROR] Invalide list object\n");
 		return;
 	}
-	printf("[*] Size of the Python list = %lu\n", ((PyVarObject *)p)->ob_size);
+	printf("[*] Size of the Python list = %lu\n",
+			((PyVarObject *)p)->ob_size);
 	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
 	for (i = 0; i < ((PyVarObject *)p)->ob_size; i++)
 	{
-		printf("Element %d: %s\n", i, ((PyListObject *)p)->ob_item[i]->ob_type->tp_name);
+		printf("Element %d: %s\n", i,
+				((PyListObject *)p)->ob_item[i]->ob_type->tp_name);
 		if (!strcmp(((PyListObject *)p)->ob_item[i]->ob_type->tp_name, "bytes"))
 			print_python_bytes(((PyListObject *)p)->ob_item[i]);
 		else if (!strcmp(((PyListObject *)p)->ob_type->tp_name, "float"))
