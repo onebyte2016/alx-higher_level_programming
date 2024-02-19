df0c59a8db80345db481f852205547dc2cf6a85b
onebyte2016 <onebyte2016@gmail.com>
2024-02-19 02:33:39 -0800

    statefilter

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -2,16 +2,22 @@
 """ prints the first State object from the database hbtn_0e_6_usa
 """
 import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 
 if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <username> <password> <database>")
+        sys.exit(1)
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
+                           .format(username, password, database))
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in session.query(State).filter(State.name.like('%a%')):
-        print(instance.id, instance.name, sep=": ")
+    states_with_a = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    for state in states_with_a:
+        print("{}: {}".format(state.id, state.name))
+    session.close()
