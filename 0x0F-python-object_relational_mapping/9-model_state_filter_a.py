95b8231431ddc9302d9267eb461bbd8639374f68
onebyte2016 <onebyte2016@gmail.com>
2024-02-19 02:36:37 -0800

    statefilter

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,23 +1,30 @@
 #!/usr/bin/python3
-""" prints the first State object from the database hbtn_0e_6_usa
 """
-import sys
+This script lists all State objects
+that contain the letter `a`
+from the database `hbtn_0e_6_usa`.
+"""
+
+from sys import argv
+from model_state import State, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python script.py <username> <password> <database>")
-        sys.exit(1)
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    """
+    Access to the database and get a state
+    from the database.
+    """
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(username, password, database))
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
+
+    engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)
+
     session = Session()
-    states_with_a = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
-    for state in states_with_a:
-        print("{}: {}".format(state.id, state.name))
-    session.close()
+
+    states = session.query(State).filter(State.name.contains('a'))
+    if states is not None:
+        for state in states:
+            print('{0}: {1}'.format(state.id, state.name))
